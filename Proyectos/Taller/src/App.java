bab38a335bc32626d4a3503908afb7c84ecd48a6
Cris-689 <cristian.artero.mol@gmail.com>
2023-09-26 10:49:45 +0200

    eliminado "propietario" del paquete coche y la cla se Vehiculo

--- Proyectos/Taller/src/App.java
+++ Proyectos/Taller/src/App.java
@@ -14,14 +14,13 @@ public class App {
         System.out.println("ciudad: " + p2.ciudad);
         System.out.println("Edad: " + p2.obtenerFecha(2023) + " años");
         System.out.println("-------------------------");
-        Vehiculo v1 = new Vehiculo("Seat", "Ibiza", 12000, "1234qwe","Ana");
-        System.out.println("nombre: " + p2.nombre);
+        Vehiculo v1 = new Vehiculo("Seat", "Ibiza", 12000, "1234qwe");
         System.out.println("Marca: " + v1.marca);
         System.out.println("Modelo: " + v1.modelo);
         System.out.println("Precio: " + v1.precio);
         System.out.println("Matricula: " + v1.matricula);
         System.out.println("-------------------------");
-        Vehiculo v2 = new Vehiculo("Toyota", "Corola", 40000, "1234asd", "Antonio");
+        Vehiculo v2 = new Vehiculo("Toyota", "Corola", 40000, "1234asd");
         System.out.println("Marca: " + v2.marca);
         System.out.println("Modelo: " + v2.modelo);
         System.out.println("Precio: " + v2.precio);
--- Proyectos/Taller/src/coche/Vehiculo.java
+++ Proyectos/Taller/src/coche/Vehiculo.java
@@ -7,12 +7,11 @@ public class Vehiculo {
     public String matricula;
     public String propietario;
 
-    public Vehiculo(String mar, String mod, int pre, String mat, String pro) {
+    public Vehiculo(String mar, String mod, int pre, String mat) {
         this.marca = mar;
         this.modelo = mod;
         this.precio = pre;
         this.matricula = mat;
-        this.propietario = pro;
     }
 
     public static void main(String[] args) {
